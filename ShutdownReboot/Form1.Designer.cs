2302aa04fa3a4e944ecd9fd755f967b69a0a8af6
Bjorn <bjoberg@signicat.com>
2017-09-12 12:21:35 +0200

    Add project files.

---
+++ ShutdownReboot/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ShutdownReboot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonShutdown = new System.Windows.Forms.Button();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.labelTimeLeft = new System.Windows.Forms.Label();
+            this.labelUptime = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // buttonShutdown
+            // 
+            this.buttonShutdown.Location = new System.Drawing.Point(23, 33);
+            this.buttonShutdown.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonShutdown.Name = "buttonShutdown";
+            this.buttonShutdown.Size = new System.Drawing.Size(97, 37);
+            this.buttonShutdown.TabIndex = 0;
+            this.buttonShutdown.Text = "Shutdown";
+            this.buttonShutdown.UseVisualStyleBackColor = true;
+            this.buttonShutdown.Click += new System.EventHandler(this.buttonShutdown_Click);
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Location = new System.Drawing.Point(133, 33);
+            this.buttonRestart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(97, 37);
+            this.buttonRestart.TabIndex = 1;
+            this.buttonRestart.Text = "Reboot";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // labelTimeLeft
+            // 
+            this.labelTimeLeft.AutoSize = true;
+            this.labelTimeLeft.Location = new System.Drawing.Point(9, 105);
+            this.labelTimeLeft.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTimeLeft.Name = "labelTimeLeft";
+            this.labelTimeLeft.Size = new System.Drawing.Size(70, 13);
+            this.labelTimeLeft.TabIndex = 3;
+            this.labelTimeLeft.Text = "labelTimeLeft";
+            this.labelTimeLeft.Visible = false;
+            // 
+            // labelUptime
+            // 
+            this.labelUptime.AutoSize = true;
+            this.labelUptime.Location = new System.Drawing.Point(9, 119);
+            this.labelUptime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelUptime.Name = "labelUptime";
+            this.labelUptime.Size = new System.Drawing.Size(62, 13);
+            this.labelUptime.TabIndex = 4;
+            this.labelUptime.Text = "labelUptime";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.SystemColors.MenuHighlight;
+            this.linkLabel1.Location = new System.Drawing.Point(207, 91);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Cancel";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 141);
+            this.ControlBox = false;
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.labelUptime);
+            this.Controls.Add(this.labelTimeLeft);
+            this.Controls.Add(this.buttonRestart);
+            this.Controls.Add(this.buttonShutdown);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonShutdown;
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.Label labelTimeLeft;
+        private System.Windows.Forms.Label labelUptime;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ ShutdownReboot/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ShutdownReboot
+{
+    public partial class Form1 : Form
+    {
+        private Timer countdown = new Timer();
+        private string action = "rebooting";
+        private int secs = 5;
+        private Boolean countdown_started = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //window position
+            var desktopWorkingArea = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea;
+            this.Left = desktopWorkingArea.Left;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+
+            //uptime
+            PerformanceCounter upTime = new PerformanceCounter("System", "System Up Time");
+            upTime.NextValue();
+            TimeSpan ts = TimeSpan.FromSeconds(upTime.NextValue());
+
+            labelUptime.Text = "Uptime: " + String.Format("{0}d {1}h {2}m", ts.Days, ts.Hours, ts.Minutes);
+        }
+
+        private void init_countdown()
+        {
+            countdown.Tick += new EventHandler(countdown_Tick);
+            countdown.Interval = (1000) * (1);
+            countdown.Enabled = countdown_started = true;
+            countdown.Start();
+        }
+
+        private void countdown_Tick(object sender, EventArgs e)
+        {
+            secs -= 1;
+            labelTimeLeft.Text = action + " in " + secs + " seconds";
+            labelTimeLeft.Visible = true;
+            if (secs <= 0)
+            {
+                countdown.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void buttonRestart_Click(object sender, EventArgs e)
+        {
+            action = "Rebooting";
+            buttonShutdown.Enabled = false;
+            //System.Diagnostics.Process.Start("ShutDown", "-r -t 5"); //restart
+            process_start_hidden("ShutDown", "-r -t 5");
+            init_countdown();
+        }
+
+        private void buttonShutdown_Click(object sender, EventArgs e)
+        {
+            action = "Shutting down";
+            buttonRestart.Enabled = false;
+            //System.Diagnostics.Process.Start("ShutDown", "-s -t 5"); //shutdown
+            process_start_hidden("ShutDown", "-s -t 5");
+            init_countdown();
+        }
+
+        private void process_start_hidden(string processname, string param1)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(processname, param1);
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process.Start(startInfo);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (countdown_started)
+            {
+                //System.Diagnostics.Process.Start("ShutDown", "-a");
+                process_start_hidden("ShutDown", "-a");
+            }
+            this.Close();
+        }
+    }
+}
